5ee631e5444c52652be81e33f74f3df30b3ba551
Albert Szabó Tönki <asztonki@renewtech.co>
2020-12-10 09:31:24 +0100

    tasks draft comments added

---
+++ script.js
@@ -0,0 +1,10 @@
+function pageLoad () {
+  
+// to do app készitése. alapértelmezésben 10 üres mező áll rendelkezésre, de tudunk adni hozzá újjakat. minden sorban van egy tickbox
+// van egy text rész és van egy törlés gomb amivel el lehet távolitani
+
+
+console.log('50');
+}
+
+window.addEventListener('load', pageLoad);
